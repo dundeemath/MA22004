eae210b70d407ab40115d289944a613d3b872fb3
Eric Hall <hall@uq.rwth-aachen.de>
2020-09-25 17:22:36 +0200

    Added infer example large sample mean hypothesis test (one-sided).

---
+++ snippets/infer.R
@@ -0,0 +1,46 @@
+library(infer)
+
+
+mu_hat <- gss %>%
+ specify(response = hours) %>%
+ calculate(stat = "mean")
+
+null_dist <- gss %>%
+ specify(response = hours) %>%
+ hypothesise(null = "point", mu = 40) %>%
+ generate(reps = 1000, type = "bootstrap") %>%
+ calculate(stat = "mean")
+
+null_dist %>%
+ visualise() +
+ shade_p_value(obs_stat = mu_hat, direction = "two-sided")
+
+p_value <- null_dist %>%
+ get_p_value(obs_stat = mu_hat, direction = "two-sided")
+p_value
+
+null_dist %>%
+ get_confidence_interval(point_estimate = mu_hat, level = 0.95, type = "se")
+
+data("trees")
+
+mu_hat <- trees %>% 
+ specify(response = Height) %>% 
+ calculate(stat = "mean")
+
+data("iris")
+
+mu_hat <- iris %>%
+ specify(response = Petal.Length) %>%
+ calculate(stat = "mean")
+
+null_dist <- iris %>%
+ specify(response = Petal.Length) %>%
+ hypothesise(null = "point", mu = 3.5) %>%
+ generate(reps = 1000, type = "bootstrap") %>%
+ calculate(stat = "mean")
+
+null_dist %>%
+ visualise() +
+ shade_p_value(obs_stat = mu_hat, direction = "two-sided")
+
